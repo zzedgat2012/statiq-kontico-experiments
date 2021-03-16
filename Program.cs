b9af48e11b8a3d38be6b8561c3e92e71ca7afb9f
Edgar Ferreira <efsn88@outlook.com>
2021-03-15 22:27:45 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using FromZeroToHero.Models;
+using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+namespace FromZeroToHero
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+          await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .ConfigureServices((services, config) =>
+            {
+                // Add the type provider
+                services.AddSingleton<ITypeProvider, CustomTypeProvider>();
+                // Configure Delivery SDK
+                services.AddDeliveryClient((IConfiguration)config);
+            })
+            .RunAsync();
+    }
+}
